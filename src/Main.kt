be38f1d346bb9884eab768a24ae6e75899215b1d
waimea-ppiasupan <168692134+waimea-ppiasupan@users.noreply.github.com>
2024-10-22 09:55:45 +1300

    a

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,13 @@
 /**
  * ------------------------------------------------------------------------
- * PROJECT NAME HERE
+ * PROJECT: Two-player game programing Task
  * Level 2 programming project
  *
- * by YOUR NAME HERE
+ * by Phanumat Piasupan
  *
- * BRIEF PROJECT DESCRIPTION HERE
- * BRIEF PROJECT DESCRIPTION HERE
- * BRIEF PROJECT DESCRIPTION HERE
+ * This project is a two player game where the aim is to win by being the player who removes the gold coin.
+ * A number of coins are placed at random positions on a one-dimensional, square grid.
+ * One of the coins is a gold coin.
  * ------------------------------------------------------------------------
  */
 
@@ -15,7 +15,60 @@
 /**
  * Program entry point
  */
+
 fun main() {
-    println("Hello World!")
+    val coin = mutableListOf<String>()
+    val nums = mutableListOf<String>()
+    val player = mutableListOf<String>()
+    coin.add("")
+    coin.add("")
+    coin.add("\uD83E\uDE99")
+    coin.add("\uD83D\uDFE1")
+    coin.add("")
+    coin.add("")
+    coin.add("\uD83D\uDFE1")
+    coin.add("")
+    coin.add("")
+    coin.add("\uD83D\uDFE1")
+
+    player.add("player 1")
+    player.add("player 2")
+
+    SetupGame(coin)
+    showGameState(coin)
+    getPlayerMove(coin)
+
+    showPetsAsHorizontalTable(coin)
 }
 
+
+
+fun SetupGame(coin: MutableList<String>) {
+
+
+}
+fun showGameState(coin: MutableList<String>) {
+
+}
+fun getPlayerMove(coin: MutableList<String>) {
+
+}
+
+// very basic horizontal output
+
+//A vertical table of our pets
+
+fun showPetsAsHorizontalTable(petList:MutableList<String>) {
+    //build the top
+    print("+----------".repeat(petList.size))
+    println("+")
+
+    //show the pets
+    for (pet in petList) {
+        print("| %-8s ".format(pet) )
+    }
+    println("|")
+    //build the bottom
+    print("+----------".repeat(petList.size))
+    println("+")
+}
